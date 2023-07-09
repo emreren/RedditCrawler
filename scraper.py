5ea079913ffafa62d884c5fe53e731f0e295a1dc
Emre Eren <emreren@hotmail.com>
2023-07-09 15:30:24 +0300

    fix: importlar düzeltildi

--- scraper.py
+++ scraper.py
@@ -1,11 +1,11 @@
-import asyncio
-from playwright.async_api import async_playwright
 from bs4 import BeautifulSoup
-from database.database import SessionLocal, engine
-from database.models import Base, Post
+from playwright.async_api import async_playwright
+
+from database.database import SessionLocal
+from database.models import Post
+
 
 async def login_and_scrape():
-
     # Veritabanı bağlantısı ve oturumu oluşturma
     session = SessionLocal()
 
@@ -17,19 +17,19 @@ async def login_and_scrape():
         await page.goto("https://www.reddit.com/login")
         await page.fill('input[name="username"]', username)
         await page.fill('input[name="password"]', password)
-        await page.click('button[type="submit"]')        
+        await page.click('button[type="submit"]')
         try:
             await page.wait_for_load_state('networkidle', timeout=60000)
         except:
-            pass        
-        
-        # Giriş yapıldıktan sonra hedef subreddit sayfasına gidin
-        await page.goto(f'https://reddit.com/r/{subreddit_name}/new',timeout=60000)
+            pass
+
+            # Giriş yapıldıktan sonra hedef subreddit sayfasına gidin
+        await page.goto(f'https://reddit.com/r/{subreddit_name}/new', timeout=60000)
         await page.keyboard.press('End')
         await page.wait_for_timeout(30000)
 
         # Sayfanın içeriğini alın   
-        page_content= await page.content()
+        page_content = await page.content()
         await browser.close()
 
         # BeautifulSoup kullanarak postları yakalayın
@@ -47,7 +47,7 @@ async def login_and_scrape():
             upvote_element = post.select_one('div[data-adclicklocation="background"] button[data-click-id="upvote"]')
             upvotes = upvote_element.find_next_sibling('div').get_text() if upvote_element else '0'
             if upvotes == "Vote":
-                upvotes="0"
+                upvotes = "0"
             # Extracting the author
             author_element = post.select_one('a[data-testid="post_author_link"]')
             author = author_element.get_text() if author_element else ''
@@ -63,23 +63,11 @@ async def login_and_scrape():
             print('Author:', author)
 
         session.commit()
-        session.close()         
-        
+        session.close()
+
+    # Kullanıcı adı, şifre, subreddit'i ve veritabanı dosyası adını ayarlayın
 
 
-# Kullanıcı adı, şifre, subreddit'i ve veritabanı dosyası adını ayarlayın
 username = "CHANGE_ME"
 password = "CHANGE_ME"
 subreddit_name = "cybersecurity"
-
-
-
-
-
-
-
-
-
-
-
-
